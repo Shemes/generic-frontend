9a57b4b76453cb42257c32dd126990fa18a439d6
Tomás Soler <tomas@tjsoler.me>
2017-10-22 04:25:30 +0200

    add jquery dependency (easily removable) and an example

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
+require('./vendor');
+
 document.addEventListener('DOMContentLoaded', () => {
     console.info('STARTER KIT: JS Ready to go!');
 });
+
+
+$('document').ready(() => console.info('JQUERY is also working...'));
